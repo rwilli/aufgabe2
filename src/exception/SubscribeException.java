9fa3cc26ff2143aff2a0053cf532c91d4b55c701
Rainer Willinger <rainer.willinger@aon.at>
2011-10-29 15:59:02 +0200

    improved exception handling for student subscribe/unsubscribe

---
+++ src/exception/SubscribeException.java
@@ -0,0 +1,30 @@
+package exception;
+
+/**
+ * Class for subscribe exception
+ * 
+ * @author rainer
+ *
+ */
+public class SubscribeException extends Exception {
+
+	/**
+	 * auto-generated id
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * default constructor
+	 */
+	public SubscribeException() {
+		
+	}
+
+	/**
+	 * @param arg0 the exception text
+	 */
+	public SubscribeException(String arg0) {
+		super(arg0);
+	}
+
+}
---
+++ src/exception/UnsubscribeException.java
@@ -0,0 +1,30 @@
+package exception;
+
+/**
+ * Class for unsubscribe exception
+ * 
+ * @author rainer
+ *
+ */
+public class UnsubscribeException extends Exception {
+
+	/**
+	 * auto-generated id
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * default constructor
+	 */
+	public UnsubscribeException() {
+		
+	}
+
+	/**
+	 * @param arg0 the exception text
+	 */
+	public UnsubscribeException(String arg0) {
+		super(arg0);
+	}
+
+}
